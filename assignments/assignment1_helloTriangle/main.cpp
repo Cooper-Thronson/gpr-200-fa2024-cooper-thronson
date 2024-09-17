212656be52483a537a977bfe0eaf889dbc7c8cdd
Cooper-Thronson <cooper.thronson@mymail.champlain.edu>
2024-09-16 21:30:11 -0400

    Progress at home on the tutorial
    Im a bit confused with the tutorial since it is doing different files from us and I dont know what i do and dont need.

--- assignments/assignment1_helloTriangle/main.cpp
+++ assignments/assignment1_helloTriangle/main.cpp
@@ -9,10 +9,13 @@
 const int SCREEN_WIDTH = 1080;
 const int SCREEN_HEIGHT = 720;
 
+const char* FRAG_SHADER_PATH = "assets/Shaders/fragmentShader.frag";
+const char* VERT_SHADER_PATH = "assets/Shaders/vertexShader.vert";
+
 float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	0.5f, -0.5f, 0.0f,
-	0.0f, 0.5f, 0.0f
+	-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+	0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+	0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f
 };
 
 
@@ -45,16 +48,16 @@ int main() {
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	//glEnableVertexAttribArray(1);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
 
 	//vertex shader create and compile
 	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
 
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glShaderSource(vertexShader, 1, &VERT_SHADER_PATH, NULL);
 	glCompileShader(vertexShader);
 	int success;
 	char infolog[512];
@@ -66,7 +69,7 @@ int main() {
 	}
 	//create and compile fragment shader
 	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glShaderSource(fragmentShader, 1, &FRAG_SHADER_PATH, NULL);
 	glCompileShader(fragmentShader);
 
 	//shader program
--- core/CThronson/Shader.cpp
+++ core/CThronson/Shader.cpp
@@ -1,2 +1,39 @@
-//"assets/Shaders/fragmentShader.frag
-#include "../ew/external/glad.h"
+#include "Shader.h"
+
+
+Shader::Shader(const char* vertexPath, const char* fragmentPath)
+{
+    std::string vertexCode;
+    std::string fragmentCode;
+    std::ifstream vShaderFile;
+    std::ifstream fShaderFile;
+    //exception throws
+    vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+
+    try
+    {
+        vShaderFile.open("assets/Shaders/vertexShader.vert");
+        fShaderFile.open("assets/Shaders/fragmentShader.frag");
+        std::stringstream vShaderStream, fShaderStream;
+
+        vShaderStream << vShaderFile.rdbuf();
+        fShaderStream << fShaderFile.rdbuf();
+
+        vShaderFile.close();
+        fShaderFile.close();
+    }
+
+
+
+    //exception catch
+    catch (std::ifstream::failure e)
+    {
+        std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+    }
+    //convert to C string
+    const char* vShaderCode = vertexCode.c_str();
+    const char* fShaderCode = fragmentCode.c_str();
+
+    //im confused. is this necessary?
+}
