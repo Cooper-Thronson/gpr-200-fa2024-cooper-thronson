2919401cc589f43b68fa360bd33b3279b5ab03c6
Cooper-Thronson <cooper.thronson@mymail.champlain.edu>
2024-11-11 23:35:18 -0500

    added includes
    unbreaks the includes, I have a weird linker error now

--- assignments/assignment_3/main.cpp
+++ assignments/assignment_3/main.cpp
@@ -9,6 +9,10 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include<imgui_impl_opengl3.h>
+
 #include <CThronson/Texture.h>
 #include <CThronson/Shader.h>
 
